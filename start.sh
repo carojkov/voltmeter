c9a3230956ca162b0796308ba9523ab1bdd3bddd
alex <alex@nostradamus>
2014-09-10 21:59:28 -0700

    sync

--- start.sh
+++ start.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+echo `pwd`
+
+JAVA_HOME=/opt/jdk1.7.0_65
+
+find . -name '*.class' | xargs -I {0} rm {0}
+
+$JAVA_HOME/bin/javac src/alex/voltmeter/Voltmeter.java
+
 TIME="$[30 * 60]"
-nohup java -cp src alex.voltmeter.Voltmeter /dev/ttyACM0 9999999 $TIME >>/home/alex/projects/voltmeter/output.log &
+nohup $JAVA_HOME/bin/java -cp src alex.voltmeter.Voltmeter /dev/ttyACM0 9999999 $TIME >>/home/alex/projects/voltmeter/output.log &
 
 sleep 3
 
