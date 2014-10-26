703edc9dabf6653d1a0c9a291759a4b737487875
alex <rojkov@gmail.com>
2014-10-25 17:41:02 -0700

    path fix

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ $JAVA_HOME/bin/javac src/carojkov/voltmeter/*.java
 
 args="-in /dev/ttyACM0 -start now -cycle 60 -check 5 -cycles 3"
 
-nohup $JAVA_HOME/bin/java -cp src carojkov.voltmeter.Main $args >>/home/carojkov/projects/voltmeter/output.log &
+nohup $JAVA_HOME/bin/java -cp src carojkov.voltmeter.Main $args >>/home/alex/projects/voltmeter/output.log &
 
 sleep 3
 
