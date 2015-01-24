6dabdaeeb4b9c518bc059b1806fbe6365b2b64a9
alex <alex@nostradamus>
2015-01-24 15:59:57 -0800

    logging changes 2

--- start.sh
+++ start.sh
@@ -10,7 +10,9 @@ $JAVA_HOME/bin/javac src/carojkov/voltmeter/*.java
 
 args="-in /dev/ttyACM0 -start now -cycle 60 -check 5 -cycles 3 $1 $2"
 
-nohup $JAVA_HOME/bin/java -cp src carojkov.voltmeter.Main $args >>/home/alex/projects/voltmeter/output.log &
+logging='-Djava.util.logging.config.file=logging.properties'
+
+nohup $JAVA_HOME/bin/java $logging -cp src carojkov.voltmeter.Main $args >>/home/alex/projects/voltmeter/output.log &
 
 sleep 3
 
